041607b4e879fc872b27f57c3b5a02d70ba2f218
Matthew Pelosi <mattpelosi@gmail.com>
2018-06-18 15:36:14 -0700

    added gitignore

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://hh-interview-challenge-mp.herokuapp.com");
-  
   res.header("Access-Control-Allow-Credentials", "true");
   res.header("withCredentials", true);
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
